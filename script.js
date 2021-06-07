ab254735d97f87b7b8d9029c4b87c514ab4a5ca5
Matt Dudzik <mdudzik92@gmail.com>
2021-06-07 14:45:33 -0500

    Creating second function to clear the text and have the colorsplosion photo appear at the end of the countdown.

--- script.js
+++ script.js
@@ -10,4 +10,27 @@ function setTime() {
 		// Adding the text to the h1 in the HTML via a JavaScript query selector.
 		timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
 	}, 1000);
+
+	// Making an if statement telling this function what to do when the timer ends.
+	if (secondsLeft === 0) {
+		// When seconds reach 0, clear the timer and run the sendMessage function.
+		clearInterval(timerInterval);
+		sendMessage();
+	}
 }
+
+// Setting up sendMessage function.
+function sendMessage() {
+	// When seconds reach 0 and sendMessage function runs, the countdown text clears.
+	timeEl.textContent = " ";
+
+	// Creating the new image element and putting it into a variable called imgEl.
+	var imgEl = document.createElement("img");
+
+	// Telling imgEl where to go for the picture to use, and appending it to the div with the id of "main" in the HTML.
+	imgEl.setAttribute("src", "images/image_1.jpg");
+	mainEl.appendChild(imgEl);
+}
+
+// Begin setTime function to start the whole process off.
+setTime();
