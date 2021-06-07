160563d32182ba2acf3be88236c616b9634e3c06
Matt Dudzik <mdudzik92@gmail.com>
2021-06-07 14:38:56 -0500

    Setting up first function to decrement seconds until photo appears.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 // Setting up variables to grab the HTML
 var timeEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
+
+// Setting up first function setTime() to count down seconds until photo appears.
+function setTime() {
+	// Setting setInterval function to decrement one second (1000 ms) at a time inside timerInterval variable.
+	var timerInterval = setInterval(function () {
+		secondsLeft--;
+		// Adding the text to the h1 in the HTML via a JavaScript query selector.
+		timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+	}, 1000);
+}
