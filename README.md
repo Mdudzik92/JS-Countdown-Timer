d6a66778320ad0140716a5ed31d14b21252d51ec
Matt Dudzik <mdudzik92@gmail.com>
2021-06-07 14:58:08 -0500

    Updated README.

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # JS-Countdown-Timer
+
+Simple JavaScript Countdown Timer
+
+<!-- Live link to deployed app -->
+
+Repository: https://github.com/Mdudzik92/JS-Countdown-Timer<br>
+Deployed app: https://mdudzik92.github.io/JS-Countdown-Timer/
+
+<!-- Technologies used -->
+
+JavaScript, HTML, DOM Manipulation
+
+<!-- Explanation of what the app is -->
+
+This is a simple timer app created with JavaScript using DOM Manipulation to access and write to a blank HTML file. The user opens the page and watches as the time counter decrements by one second at a time until a 'colorsplosion' photo appears. What's interesting about this is that there is nothing other than an ID and a class in the HTML before it is written via invoking the JS file's functionality.
+
+<!-- Screenshot -->
+
+<img src="./images/countdown.png">
+<img src="./images/image_1.jpg">
+
+<!-- License -->
+
+MIT
+
+<!-- Contact information -->
+
+Email: mdudzik92@gmail.com
--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 var timeEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
 
+// Adding a specific number to start the countdown from.
+var secondsLeft = 10;
+
 // Setting up first function setTime() to count down seconds until photo appears.
 function setTime() {
 	// Setting setInterval function to decrement one second (1000 ms) at a time inside timerInterval variable.
@@ -9,14 +12,14 @@ function setTime() {
 		secondsLeft--;
 		// Adding the text to the h1 in the HTML via a JavaScript query selector.
 		timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
-	}, 1000);
 
-	// Making an if statement telling this function what to do when the timer ends.
-	if (secondsLeft === 0) {
-		// When seconds reach 0, clear the timer and run the sendMessage function.
-		clearInterval(timerInterval);
-		sendMessage();
-	}
+		// Making an if statement telling this function what to do when the timer ends.
+		if (secondsLeft === 0) {
+			// When seconds reach 0, clear the timer and run the sendMessage function.
+			clearInterval(timerInterval);
+			sendMessage();
+		}
+	}, 1000);
 }
 
 // Setting up sendMessage function.
